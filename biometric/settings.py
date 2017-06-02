0a5c37b9716a5ea6007a38fdb09c490e1bbfe365
Rishabh Narang <rnradon17@gmail.com>
2017-06-02 18:01:22 +0530

    post req added to details view

--- biometric/settings.py
+++ biometric/settings.py
@@ -199,8 +199,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
